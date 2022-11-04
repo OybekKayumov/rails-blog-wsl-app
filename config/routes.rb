918c82c9b7d91a9450afc25445f8495b57d5c8c4
OybekKayumov <oybek.kayumov@gmail.com>
2022-11-04 20:10:45 +0500

    Add resources and link_to

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   # root "pages#index"
   root "articles#index"
 
-  get "articles", to: 'articles#index'
-  get "articles/:id", to: "articles#show"
+  # get "articles", to: 'articles#index'
+  # get "articles/:id", to: "articles#show"
+  resources :articles
 end
